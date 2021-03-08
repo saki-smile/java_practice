f6616d6a3c9da1254ebd38756a25f2df03c480fc
saki-smile <sa.su.ko0419@gmail.com>
2021-03-08 21:47:39 +0900

    add array

---
+++ JavaWcp/src/array1.java
@@ -0,0 +1,15 @@
+
+public class array1 {
+	public static void main(String[] args) {
+		int[] array1 = new int[3];
+		array1[0] = 60;
+		array1[1] = 20;
+		array1[2] = 30;
+		System.out.println(array1);
+		int int2 = array1[2];
+		System.out.println(int2);
+		int array2[] = null;
+		int[] array3 = new int[] { 10, 20, 50 };
+		int[] array4 = { 10, 20, 50 };
+	}
+}
