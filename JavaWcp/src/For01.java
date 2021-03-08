71c09ae510e1330796051d469e6e1f6eb0dd59d9
saki-smile <sa.su.ko0419@gmail.com>
2021-03-08 21:53:07 +0900

    add for

---
+++ JavaWcp/src/For01.java
@@ -0,0 +1,8 @@
+
+public class For01 {
+	public static void main(String[] args) {
+		for (int num = 1; num <= 5; num++) {
+			System.out.println("For01 = " + num);
+		}
+	}
+}
---
+++ JavaWcp/src/For02.java
@@ -0,0 +1,9 @@
+
+public class For02 {
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5};
+		for (int num : array) {
+			System.out.println("For02 = " + num);
+		}
+	}
+}
