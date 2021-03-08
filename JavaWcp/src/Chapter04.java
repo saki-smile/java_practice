928c31664d805f2bf6c75d9b9c7101ceba561fe7
saki-smile <sa.su.ko0419@gmail.com>
2021-03-08 21:44:58 +0900

    add Chapter

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "saki";
+		System.out.println(name);
+		name = "yuki";
+		System.out.println(name);
+		
+		final int num = 1;
+		System.out.println(num);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 30000000000L;
+		System.out.println(long1);
+		double double1 = 3.14;
+		System.out.println(double1);
+		boolean boolean1 = 4 >= 6;
+		System.out.println(boolean1);
+		Float float1 = 1.2344F;
+		Integer integer1 = 5;
+		Character chara1 = 'h';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(chara1);
+		int[] array1 = { 10, 20, 50, 40, 60 };
+		System.out.println(array1[3]);
+		
+	}
+}
